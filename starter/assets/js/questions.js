e7312b4eb688c1e0689cf828250b78472d151537
kabuzo13 <beahonigmann@gmail.com>
2024-01-15 14:54:00 +0000

    added questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,16 @@
+var questions = [
+    {
+        title: "What is the capital of France?",
+        choices: ["Madrid", "Lille", "Paris", "Berlin"],
+        answer: "Paris",
+    },
+    {
+        title: "What is the capital of Italy?",
+        choices: ["Rome", "Venice", "Pisa", "Naples"],
+        answer: "Rome",
+    },
+    {
+        title: "What is the capital of Germany?",
+        choices: ["Leipzig", "Berlin", "Munich", "Hamburg"],
+    }
+]
